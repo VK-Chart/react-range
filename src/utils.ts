a530acc7587e0b595c1855532d55c81d0301e57f
Aleksandr Kuksov <askuksov@gmail.com>
2024-11-05 00:44:15 +0200

    Fix error message

--- src/utils.ts
+++ src/utils.ts
@@ -69,7 +69,7 @@ export function isVertical(direction: Direction) {
 
 export function checkBoundaries(value: number, min: number, max: number) {
   if (min > max) {
-    throw new RangeError(`min (${min}) is equal/bigger than max (${max})`);
+    throw new RangeError(`min (${min}) is bigger than max (${max})`);
   }
   if (value < min) {
     throw new RangeError(`value (${value}) is smaller than min (${min})`);
